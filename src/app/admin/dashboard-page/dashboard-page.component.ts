2e059b65035c81975fec79a4b56a9e27c128b194
Zhanna <nesterova.zhannet@gmail.com>
2023-04-12 13:32:43 +0500

    dashboard page creation

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from 'src/app/shared/interfaces';
+import { PostsService } from 'src/app/shared/posts.service';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent {
+export class DashboardPageComponent implements OnInit, OnDestroy{
 
-  constructor(){
+  posts: Post[] = []
+  pSub!: Subscription
+
+  constructor(private postsService: PostsService){
 
   }
- 
+  
+  ngOnInit(): void {
+    this.pSub = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.pSub.unsubscribe()
+  }
+
+  remove(id: string){
+
+  }
 }
