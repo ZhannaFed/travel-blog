31c630bc85bd8d45fa24401c9370cd2b709f651a
Zhanna <nesterova.zhannet@gmail.com>
2023-04-13 14:08:26 +0500

    editing post

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { Subscription, switchMap } from 'rxjs';
 import { Post } from 'src/app/shared/interfaces';
 import { PostsService } from 'src/app/shared/posts.service';
 
@@ -10,22 +10,26 @@ import { PostsService } from 'src/app/shared/posts.service';
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy {
   
   form!: FormGroup
+  post!: Post
+  submitted = false
+  uSub!: Subscription
 
   constructor(
     private route: ActivatedRoute,
     private postsService: PostsService 
     ) {
   }
-  
+    
   ngOnInit(): void {
     this.route.params.pipe(
       switchMap((params: Params) => {
         return this.postsService.getById(params['id'])
       })
     ).subscribe((post: Post) => {
+      this.post = post
       this.form = new FormGroup({
         title: new FormControl(post.title, Validators.required),
         img: new FormControl(post.img, Validators.required),
@@ -34,7 +38,27 @@ export class EditPageComponent implements OnInit {
     })
   }
 
+  ngOnDestroy(): void {
+    if(this.uSub){
+      this.uSub.unsubscribe()
+    }
+  }
+
   submit(){
+    if(this.form.invalid){
+      return
+    }
+
+    this.submitted = true
+
+    this.uSub = this.postsService.update({
+      ...this.post,
+      title: this.form.value.title,
+      img: this.form.value.img,
+      text: this.form.value.text,
+    }).subscribe(() => {
+      this.submitted = false
+    })
 
   }
 
