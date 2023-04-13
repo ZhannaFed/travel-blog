b5d023db14b719c17939a1e2ef9e79580faff55c
Zhanna <nesterova.zhannet@gmail.com>
2023-04-13 15:45:52 +0500

    message plugin creation

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription, switchMap } from 'rxjs';
 import { Post } from 'src/app/shared/interfaces';
 import { PostsService } from 'src/app/shared/posts.service';
+import { AlertService } from '../shared/services/alert.service';
 
 @Component({
   selector: 'app-edit-page',
@@ -19,7 +20,8 @@ export class EditPageComponent implements OnInit, OnDestroy {
 
   constructor(
     private route: ActivatedRoute,
-    private postsService: PostsService 
+    private postsService: PostsService,
+    private alert: AlertService
     ) {
   }
     
@@ -58,6 +60,8 @@ export class EditPageComponent implements OnInit, OnDestroy {
       text: this.form.value.text,
     }).subscribe(() => {
       this.submitted = false
+      this.alert.success('Пост был обновлен')
+
     })
 
   }
